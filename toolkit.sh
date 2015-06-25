eb06817804ed5ad4a7da85e454d23ce3eda48517
HustLion <hustlionM@qq.com>
2015-06-24 20:55:45 -0700

    Create toolkit.sh

---
+++ toolkit.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# basic tools
+sudo apt-get install git
+sudo apt-get install vim-gnome
+sudo apt-get install curl
+sudo apt-get install gitk
+sudo apt-get install pandoc
+
+# Autotools
+sudo apt-get install build-essential
+sudo apt-get install autopoint
+sudo apt-get install automake
+sudo apt-get install autoconf
+sudo apt-get install gettext-base gettext liblocale-gettext-perl
+
+# mentohust specific
+sudo apt-get install libpcap-dev libnotify-dev
+
+# compiler
+sudo apt-get install g++
