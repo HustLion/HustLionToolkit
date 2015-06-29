006168259bab78cc11e3271ef4e129115b479883
HustLion <hustlionM@qq.com>
2015-06-29 10:54:35 +0800

    Update git.sh

--- usage/git.sh
+++ usage/git.sh
@@ -1,3 +1,15 @@
+# ssh
+ssh-keygen -t rsa -b 4096 -C "hustlionM@qq.com"
+ssh-agent -s
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+clip < ~/.ssh/id_rsa.pub
+ssh -T git@github.com
+
+git config --global user.name "HustLion"
+git config --global user.email "hustlionM@qq.com"
+
+
 git submodule update --init --recursive
 git submodule add https://github.com/HustLion/snake
 
